fe4d8bd33e9760bddcd7e89f590be3728c098d94
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2026-07-02 01:15:58 +0200

    Upgraded solidity compiler version to 0.8.24 to match openzeppelin specifications

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.24", // Matches the pragma in your smart contract
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "cancun", // Explicitly sets the target VM to enable mcopy
+    },
+  },
 };
